bf39b7335167e06a7be72c46cd080d59a26c12ec
Atmajo <atmajoc@gmail.com>
2023-10-17 21:22:40 +0530

    updates

--- src/components/partials/App.jsx
+++ src/components/partials/App.jsx
@@ -8,6 +8,7 @@ import feels from '../assests/feels.png'
 
 const App = () => {
   const api = "7e3cfe148314588f0fb226f55d5943f4";
+  const api2 = "cd8ab197a16b48e595a50816231410";
 
   const search = async () => {
     const input = document.getElementsByClassName("input");
@@ -15,9 +16,13 @@ const App = () => {
       return 0;
     } else {
       const URL = `https://api.openweathermap.org/data/2.5/weather?appid=${api}&q=${input[0].value}&units=metric`;
+      const URL2 = `http://api.weatherapi.com/v1/current.json?key=${api2}&q=${input[0].value}&aqi=no`
       const response = await fetch(URL);
       const data = await response.json();
       console.log(data)
+
+      const response2 = await fetch(URL2);
+      const data2 = await response2.json();
 
       const humidity = document.getElementsByClassName("humidity");
       const wind = document.getElementsByClassName("wind");
@@ -26,7 +31,7 @@ const App = () => {
 
       humidity[0].innerHTML = Math.ceil(data.main.humidity);
       wind[0].innerHTML = Math.ceil(data.wind.speed);
-      uv[0].innerHTML = Math.ceil(data.main.humidity);
+      uv[0].innerHTML = (data2.current.uv);
       feels[0].innerHTML = Math.ceil(data.main.feels_like);
 
       const temp = document.getElementsByClassName("temp");
@@ -37,67 +42,69 @@ const App = () => {
 
       const place = document.getElementsByClassName("place");
 
-      place[0].innerHTML = input[0].value;
+      place[0].innerHTML = data.name;
     }
   }
 
   return (
-    <div className="w-[340px] flex flex-col justify-center align-middle m-auto mt-20 bg-zinc-200 rounded-xl select-none">
-      <div className="flex gap-5 my-10 p-5 bg-green-300 rounded-lg">
-        <h1 className="font-mono">WEATHER</h1>
-        <div className="flex gap-2">
-          <input type="text" className="input font-mono border-none outline-none rounded-lg"/>
-          <button >
-            <IoSearchOutline className="rounded-xl" onClick={search}/>
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col mb-5 p-5 bg-green-300 rounded-lg">
-        <div className="gap-5 flex">
-          <div>
-            <img src={o1d} alt="" />
-          </div>
-          <div className="py-5">
-            <h1 className="temp text-5xl">20℃</h1>
-            <p className="type text-xl">Clear Sky</p>
-            <p className="place text-xl">India</p>
+    <div className="w-[360px] flex flex-col justify-center align-middle m-auto mt-20 bg-zinc-200 rounded-xl select-none">
+      <div className="w-[340px] m-auto">
+        <div className="flex gap-5 my-10 p-5 bg-green-300 rounded-xl">
+          <h1 className="font-mono">WEATHER</h1>
+          <div className="flex gap-2">
+            <input type="text" className="input font-mono border-none outline-none rounded-lg"/>
+            <button >
+              <IoSearchOutline className="rounded-xl" onClick={search}/>
+            </button>
           </div>
         </div>
-        <div className="flex">
-          <div className="flex justify-between my-5 p-5 gap-10">
-            <div className="flex gap-2">
-              <img className="h-[42px]" src={humidity} alt="" />
-              <div>
-                <h1 className="humidity text-xl">51%</h1>
-                <p>Humidity</p>
-              </div>
+        <div className="flex flex-col mb-5 p-5 bg-green-300 rounded-xl">
+          <div className="gap-5 flex">
+            <div>
+              <img src={o1d} alt="" />
             </div>
-            <div className="flex gap-2">
-             <img className="h-[42px]" src={wind} alt="" />
-             <div>
-                <h1 className="wind text-xl">18kph</h1>
-                <p>Wind</p>
+            <div className="py-5">
+              <h1 className="temp text-5xl">20℃</h1>
+              <p className="type text-xl">Clear Sky</p>
+              <p className="place text-xl">India</p>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-between my-5 p-5 gap-10">
+              <div className="flex gap-2">
+                <img className="h-[42px]" src={humidity} alt="" />
+                <div>
+                  <h1 className="humidity text-xl">51%</h1>
+                  <p>Humidity</p>
+                </div>
+              </div>
+              <div className="flex gap-2">
+              <img className="h-[42px]" src={wind} alt="" />
+              <div>
+                  <h1 className="wind text-xl">18kph</h1>
+                  <p>Wind</p>
+                </div>
               </div>
             </div>
           </div>
+          <div className="flex">
+            <div className="flex justify-between my-5 p-5 gap-20">
+              <div className="flex gap-2">
+                <img className="h-[42px]" src={uv} alt="" />
+                <div>
+                  <h1 className="uv text-xl">3</h1>
+                  <p>UV</p>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                <img className="h-[42px]" src={feels} alt="" />
+                <div>
+                  <h1 className="feels text-xl">25°</h1>
+                  <p>Feels</p>
+                </div>
+              </div>
+            </div>
         </div>
-        <div className="flex">
-          <div className="flex justify-between my-5 p-5 gap-20">
-            <div className="flex gap-2">
-              <img className="h-[42px]" src={uv} alt="" />
-              <div>
-                <h1 className="uv text-xl">3</h1>
-                <p>UV</p>
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <img className="h-[42px]" src={feels} alt="" />
-              <div>
-                <h1 className="feels text-xl">25°</h1>
-                <p>Feels</p>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
