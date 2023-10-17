b7d80f1f87a34148f42269222c713488dd1b9348
Atmajo <atmajoc@gmail.com>
2023-10-17 23:24:27 +0530

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,6 @@ const App = () => {
       return 0;
     } else {
       const URL = `https://api.openweathermap.org/data/2.5/weather?appid=${api}&q=${input[0].value}&units=metric`;
-      const URL2 = `http://api.weatherapi.com/v1/current.json?key=${api2}&q=${input[0].value}&aqi=no`
       const response = await fetch(URL);
       const data = await response.json();
 
